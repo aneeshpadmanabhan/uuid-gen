f6abea876aa651a86e6944631b2c55c549adba2f
Aneesh Padmanabhan <aneeshpadmanabhan@gmail.com>
2020-09-07 19:14:01 -0700

    Support for command line arg and env variable for refresh interval

--- loop.sh
+++ loop.sh
@@ -4,9 +4,10 @@ interval=$INTERVAL
 [ -z "$interval" ] && interval=$1
 [ -z "$interval" ] && interval=5
 echo "interval = ${interval}"
+mkdir htdocs
 while :
 do
     echo "Generating new uuid..."
-    uuidgen
+    uuidgen > htdocs/index.html
     sleep $interval
 done
