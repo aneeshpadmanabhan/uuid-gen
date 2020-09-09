62ebfb3dd6ccec1dfce3ba698ccd3aa62fe07a51
Aneesh Padmanabhan <aneeshpadmanabhan@gmail.com>
2020-09-08 17:17:09 -0700

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -4,10 +4,10 @@ interval=$INTERVAL
 [ -z "$interval" ] && interval=$1
 [ -z "$interval" ] && interval=5
 echo "interval = ${interval}"
-mkdir htdocs
+[ ! -d "/htdocs" ] && mkdir /htdocs
 while :
 do
     echo "Generating new uuid..."
-    uuidgen > htdocs/index.html
+    uuidgen > /htdocs/index.html
     sleep $interval
 done
